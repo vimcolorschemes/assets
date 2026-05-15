3a494f5c74b5e840e1f1cd0ca5fe0f3efb52155a
Robin Gagnon <me@reobin.dev>
2026-05-15 16:12:27 -0500

    chore: add bit generation scripts

---
+++ generate-vimcolorschemes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+
+bit \
+  -font pressstart \
+  -color "#7fb4ca" \
+  -gradient "#98bb6c" \
+  -direction right \
+  -shadow \
+  -shadow-style 0 \
+  "vimcolorschemes"
